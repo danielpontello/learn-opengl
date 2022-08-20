8224f16ada6223fe1ac6c435ec77264db594b492
Daniel Sader <danielpontello@gmail.com>
2022-08-20 04:32:01 +0100

    Hello Window

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -1,13 +1,77 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+// Callback for window resizing
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+// Input processing
+void processInput(GLFWwindow* window)
+{
+    // Check if escape key was pressed
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main()
 {
+    // Initialize GLFW for OpenGL 3.3, core profile
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Create a window and set it as the context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Load GLAD function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Set the OpenGL viewport
+    glViewport(0, 0, 800, 600);
+
+    // Bind the callback function for resizing
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Process any input
+        processInput(window);
+
+        // Set color for clearing screen
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // Clear the color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+        // Poll events (input, etc.)
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glfwTerminate();
 
     return 0;
 }
